9b161c6bb45790bfa71a1cc04bbf4260fb64ee79
gigavisions <gigavisions@gmail.com>
2025-09-04 13:11:53 +0530

    cors policy added

--- src/main/java/com/example/ucms/authConfigerations/CorsConfig.java
+++ src/main/java/com/example/ucms/authConfigerations/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","https://68b9411bb5f2a50008b95e7c--quiet-phoenix-f1ae33.netlify.app","https://68b9411bb5f2a50008b95e7c--quiet-phoenix-f1ae33.netlify.app/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
